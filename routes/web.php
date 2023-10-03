084a2d284e267af83ec967bf7707d1e2365735e2
DarioEsposito89 <darioesposito.work@gmail.com>
2023-10-03 18:10:54 +0200

    inserito commenti

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,37 @@ use App\Http\Controllers\ComicController;
 |
 */
 
+// HOME
 Route::get('/', function () {
     return view('home');
 })->name('home');
 
+// INDEX
 Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
+
+// CREATE
 Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
+
+// SHOW
 Route::get('/comics/{id}', [ComicController::class, 'show'])->name('comics.show');
+
+// STORE
 Route::post('/comics',[ComicController::class, 'store'])->name('comics.store');
+
+// EDIT
 Route::get('/comics/{comic}/edit',[ComicController::class, 'edit'])->name('comics.edit');
+
+// UPDATE
 Route::put('/comics/{comic}',[ComicController::class, 'update'])->name('comics.update');
+
+// DESTROY
 Route::delete('/comics/{comic}', [ComicController::class, 'destroy'])->name('comics.destroy');
 
 
 
 
 
-
+// OTHERS ROOUTE LINK
 Route::get('/characters', function () {
     return view('characters');
 })->name('characters');
